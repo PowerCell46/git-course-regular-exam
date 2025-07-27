94e2da439e5acfc1a92b042af3d74b3a40cec5ab
Peter Gerdzhikov <peter.gerdzhikov.contact@gmail.com>
2025-07-27 11:08:19 +0300

    TASK-3: Adding pre-push mocha&chai testing

---
+++ Js/test/utils.test.js
@@ -0,0 +1,24 @@
+const { expect } = require("chai");
+const { sum, subtract } = require("../utils");
+
+describe("Utils", () => {
+  describe("sum()", () => {
+    it("should return correct sum of two numbers", () => {
+      expect(sum(2, 3)).to.equal(5);
+    });
+
+    it("should work with negative numbers", () => {
+      expect(sum(-2, -3)).to.equal(-5);
+    });
+  });
+
+  describe("subtract()", () => {
+    it("should return correct difference of two numbers", () => {
+      expect(subtract(5, 3)).to.equal(2);
+    });
+
+    it("should work with negative numbers", () => {
+      expect(subtract(-5, -2)).to.equal(-3);
+    });
+  });
+});
