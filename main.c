c4e54f44b743b753b42b89cda08e128b01840ade
Peter Gerdzhikov <peter.gerdzhikov.contact@gmail.com>
2025-07-27 09:47:42 +0300

    Add end of program msg

--- main.c
+++ main.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 
 #define PROGRAMMER_NAME "Peter"
+#define LINE_SEPARATOR "----"
 
 
 int main()
 {
-    printf("Start of the C written program.\n----\n");
-    printf("Name of the creator: %s.", PROGRAMMER_NAME);
+    printf("Start of the C written program.\n%s\n", LINE_SEPARATOR);
+    printf("Name of the creator: %s.\n", PROGRAMMER_NAME);
+    printf("%s\nEnd of the C program.\n", LINE_SEPARATOR);
 
     return 0;
 }
