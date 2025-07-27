f9db63bb72426b8214c4c043d941ed76f4c34d33
Peter Gerdzhikov <peter.gerdzhikov.contact@gmail.com>
2025-07-27 09:50:23 +0300

    Adding begin&end of file separators

--- main.c
+++ main.c
@@ -2,13 +2,14 @@
 
 #define PROGRAMMER_NAME "Peter"
 #define LINE_SEPARATOR "----"
+#define BEGIN_END_PROGRAM_SEPARATOR "````"
 
 
 int main()
 {
-    printf("Start of the C written program.\n%s\n", LINE_SEPARATOR);
+    printf("%s\nStart of the C written program.\n%s\n", BEGIN_END_PROGRAM_SEPARATOR, LINE_SEPARATOR);
     printf("Name of the creator: %s.\n", PROGRAMMER_NAME);
-    printf("%s\nEnd of the C program.\n", LINE_SEPARATOR);
+    printf("%s\nEnd of the C program.\n%s\n", LINE_SEPARATOR, BEGIN_END_PROGRAM_SEPARATOR);
 
     return 0;
 }
